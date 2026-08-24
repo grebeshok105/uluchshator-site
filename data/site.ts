454360105e8e04fa37bc5221383393e18ad8d207
grebeshok105 <grebeshok105@users.noreply.github.com>
2026-08-24 18:41:40 +0300

    feat: sample content and site data

---
+++ data/site.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  version: "v0.3",
+  skillCount: 56,
+  baseCount: 4,
+  metricCount: 180,
+  statuses: ["линт-гейт активен", "скиллы валидируются", "базы свежие"],
+} as const;
